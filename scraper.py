612c103a4320f3e20db6886f2a525b2300f38062
AmanBotz <amanpooniaff@gmail.com>
2024-09-19 14:02:47 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,12 +10,20 @@ def scrape_video_and_thumbnail(url):
 
     # Extract videos and thumbnails from the page
     for item in soup.find_all('div', class_="thumb-list__item video-thumb video-thumb--type-video"):
-        video_url = item.find('a', class_='video-thumb__image-container')['href']
-        if video_url.startswith('https://xhamster.com/videos/'):
-            videos.append(video_url)
+        # Safely extract video URL
+        video_link = item.find('a', class_='video-thumb__image-container')
+        if video_link and 'href' in video_link.attrs:
+            video_url = video_link['href']
+            if video_url.startswith('https://xhamster.com/videos/'):
+                videos.append(video_url)
+        else:
+            continue  # Skip if no valid video link is found
 
-        thumbnail_url = item.find('img', class_='tnum-1 thumb-image-container__image')['src']
-        if thumbnail_url.startswith('https://ic-vt-nss.xhcdn.com/'):
-            thumbnails.append(thumbnail_url)
+        # Safely extract thumbnail URL
+        thumbnail_img = item.find('img', class_='tnum-1 thumb-image-container__image')
+        if thumbnail_img and 'src' in thumbnail_img.attrs:
+            thumbnail_url = thumbnail_img['src']
+            if thumbnail_url.startswith('https://ic-vt-nss.xhcdn.com/'):
+                thumbnails.append(thumbnail_url)
 
     return videos, thumbnails
