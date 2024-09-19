3870e360878f384fbd5408e98e4d9c66d69471ac
AmanBotz <amanpooniaff@gmail.com>
2024-09-19 13:49:49 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,13 +8,14 @@ def scrape_video_and_thumbnail(url):
     videos = []
     thumbnails = []
 
-    for item in soup.find_all('div', class_='thumb-list__item'):
-        video_link = item.find('a', class_='video-thumb__image-container')['href']
-        thumbnail_link = item.find('img', class_='tnum-1 thumb-image-container__image')['src']
-        
-        if 'https://xhamster.com/videos/' in video_link and not any(video_link.endswith(ext) for ext in ['.mp4', '.avi', '.mov']):
-            videos.append(video_link)
-            thumbnails.append(thumbnail_link)
+    # Extract videos and thumbnails from the page
+    for item in soup.find_all('div', class_="thumb-list__item video-thumb video-thumb--type-video"):
+        video_url = item.find('a', class_='video-thumb__image-container')['href']
+        if video_url.startswith('https://xhamster.com/videos/'):
+            videos.append(video_url)
 
-    return list(set(videos)), list(set(thumbnails))  # Remove duplicates
+        thumbnail_url = item.find('img', class_='tnum-1 thumb-image-container__image')['src']
+        if thumbnail_url.startswith('https://ic-vt-nss.xhcdn.com/'):
+            thumbnails.append(thumbnail_url)
 
+    return videos, thumbnails
